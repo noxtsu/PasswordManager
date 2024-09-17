41de6d89cf94d36fcc51949ffb9716de3955c3f5
ryota.nozu <kuraraiban29@gmail.com>
2024-09-17 21:52:31 +0900

    Initial Commit

---
+++ PasswordManager/PasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerApp.swift
+//  PasswordManager
+//
+//  Created by ryota.nozu on 2024/09/17.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
